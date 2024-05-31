5be6ad894a27adb895aa926004c1557eed3469e9
bhargavaraman <bhargavaraman01@gmai.com>
2024-05-31 17:47:18 +0530

    Target Cart Add

---
+++ Reports/Html_Reports/report.js
@@ -0,0 +1,313 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("target.feature");
+formatter.feature({
+  "line": 1,
+  "name": "User Login Target Website and Buy a Spiderman Toy",
+  "description": "",
+  "id": "user-login-target-website-and-buy-a-spiderman-toy",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "User Login the Target Web Application",
+  "description": "",
+  "id": "user-login-target-website-and-buy-a-spiderman-toy;user-login-the-target-web-application",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "User Lanch the Target Application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User enter the Username in username field",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "User enter the Password in password field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "User click the Sign button and open to main page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Lanch_the_Target_Application()"
+});
+formatter.result({
+  "duration": 29850540355,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_Username_in_username_field()"
+});
+formatter.result({
+  "duration": 11001649440,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_Password_in_password_field()"
+});
+formatter.result({
+  "duration": 670420571,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_Sign_button_and_open_to_main_page()"
+});
+formatter.result({
+  "duration": 4432311389,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "User Search the Toy items and select age",
+  "description": "",
+  "id": "user-login-target-website-and-buy-a-spiderman-toy;user-search-the-toy-items-and-select-age",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 10,
+  "name": "User Enter the toy name in Search field",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "User clicks the age button and select age in age list menu and clicks apply button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "User Scroll up and down to view the toys item",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "select one toy name and it\u0027s open to toy details page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Enter_the_toy_name_in_Search_field()"
+});
+formatter.result({
+  "duration": 11178293446,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_the_age_button_and_select_age_in_age_list_menu_and_clicks_apply_button()"
+});
+formatter.result({
+  "duration": 12753833197,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Scroll_up_and_down_to_view_the_toys_item()"
+});
+formatter.result({
+  "duration": 8693746378,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.select_one_toy_name_and_it_s_open_to_toy_details_page()"
+});
+formatter.result({
+  "duration": 4269545740,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "User Check the Toy images,Specification Details and add to card",
+  "description": "",
+  "id": "user-login-target-website-and-buy-a-spiderman-toy;user-check-the-toy-images,specification-details-and-add-to-card",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 16,
+  "name": "User scroll down to view first toy image",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "User clicks Details down arrowmark button to view toy details and again clicks to close",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "User clicks to one by one toy Details and Specifications",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "User Clicks add to card button to add first toy and clicks to continue shop button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_scroll_down_to_view_first_toy_image()"
+});
+formatter.result({
+  "duration": 12745766518,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_Details_down_arrowmark_button_to_view_toy_details_and_again_clicks_to_close()"
+});
+formatter.result({
+  "duration": 3549736163,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_to_one_by_one_toy_Details_and_Specifications()"
+});
+formatter.result({
+  "duration": 10512910548,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Clicks_add_to_card_button_to_add_first_toy_and_clicks_to_continue_shop_button()"
+});
+formatter.result({
+  "duration": 11627395068,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 21,
+  "name": "User add another one item to add to card",
+  "description": "",
+  "id": "user-login-target-website-and-buy-a-spiderman-toy;user-add-another-one-item-to-add-to-card",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 22,
+  "name": "User scroll up to search box and enter toy name in search filed",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 23,
+  "name": "User scroll down to view toy images",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 24,
+  "name": "User clicks to one by one toy Details and Specifications",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "user Clicks add to card button to add a toy and clicks to continue shop button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_scroll_up_to_search_box_and_enter_toy_name_in_search_filed()"
+});
+formatter.result({
+  "duration": 13957641035,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_scroll_down_to_view_toy_images()"
+});
+formatter.result({
+  "duration": 19441438852,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_to_one_by_one_toy_Details_and_Specifications()"
+});
+formatter.result({
+  "duration": 9911928251,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Clicks_add_to_card_button_to_add_a_toy_and_clicks_to_continue_shop_button()"
+});
+formatter.result({
+  "duration": 16618375676,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 27,
+  "name": "User buying the items to cart page",
+  "description": "",
+  "id": "user-login-target-website-and-buy-a-spiderman-toy;user-buying-the-items-to-cart-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 28,
+  "name": "User clicks cart button and navigate to show added item details",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 29,
+  "name": "User Scroll page and view and check the added items",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 30,
+  "name": "User clicks to Order summary down arrowbutton and check total price details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "User clicks to select email a gift message click on button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "User Clicks to promo code Addbox and enter the promo code details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "User clicks to Check out all button and it\u0027s navigate to checkout page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_cart_button_and_navigate_to_show_added_item_details()"
+});
+formatter.result({
+  "duration": 6705134274,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Scroll_page_and_view_and_check_the_added_items()"
+});
+formatter.result({
+  "duration": 4217886036,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_to_Order_summary_down_arrowbutton_and_check_total_price_details()"
+});
+formatter.result({
+  "duration": 4252830974,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_to_select_email_a_gift_message_click_on_button()"
+});
+formatter.result({
+  "duration": 4615955250,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_Clicks_to_promo_code_Addbox_and_enter_the_promo_code_details()"
+});
+formatter.result({
+  "duration": 4313856018,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_clicks_to_Check_out_all_button_and_it_s_navigate_to_checkout_page()"
+});
+formatter.result({
+  "duration": 4833125242,
+  "status": "passed"
+});
+});
